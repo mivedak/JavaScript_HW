5a872f143b7cbfbd2b82345e1690ba46e21900e5
mivedak <45517399+mivedak@users.noreply.github.com>
2019-03-20 22:40:30 -0400

    Create app.js
    Push app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+// writng the data in html table
+
+tableData.forEach(function(sighting) {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+}); 
+
+var dateFilter = d3.select("#filter-btn");
+
+dateFilter.on("click", function() {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredTable = tableData.filter(dates => dates.datetime === inputValue);
+
+    d3.selectAll("td").remove();
+    filteredTable.forEach(function (sighting) {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key,value]) {
+            var cell = row.append("td");
+        });
+    });
+
+});
